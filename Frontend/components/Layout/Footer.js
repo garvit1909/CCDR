4252f711998840a4204e6129d1383211694cb1ff
garvit1909 <110663702+garvit1909@users.noreply.github.com>
2024-12-19 03:06:49 +0530

    Update Footer.js

--- Frontend/components/Layout/Footer.js
+++ Frontend/components/Layout/Footer.js
@@ -1,53 +1,75 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react';
 import Link from "next/link";
+import Image from "next/image";
 
 const Footer = () => {
     return (
-        <footer className="footer mt-50">
+        <footer className="footer bg-light text-dark pt-5 pb-4">
             <div className="container">
-                <div className="row">
-                    <div className="footer-col-6">
-                        <Link legacyBehavior href="/"><a><img alt="jobBox" src="/assets/imgs/prorep (1).png"  style={{width:"180px"}}/></a></Link>
-                        <div className="mt-20 mb-20 font-xs color-text-paragraph-2">ProRep stands at the forefront of the DTU student community, serving as the ultimate resource to explore, connect with, and discover global designers and job opportunities.</div>
-                        <div className="footer-social">
-                            <a className="icon-socials icon-facebook" href="#" />
-                            <a className="icon-socials icon-twitter" href="#" />
-                            <a className="icon-socials icon-linkedin" href="#" /></div>
-                    </div>
-                    <div className="footer-col-4 col-md- col-xs-6" style={{marginLeft:"100px"}}>
-                        <h6 className="mb-20 ml-200">Resources</h6>
-                        <ul className="menu-footer ml-100" style={{display:"flex",flexDirection:"row"}}>
-                            <li>
-                                <a href="#" style={{marginRight:"50px"}}>About us</a></li> <li>
-                                <a href="#" style={{marginRight:"50px"}}>Our Team</a></li> <li>
-                                <a href="#" style={{marginRight:"50px"}}>Products</a></li> <li>
-                                <a href="#" style={{marginRight:"50px"}}>Contact</a></li>
-                        </ul>
+                {/* Main Row */}
+                <div className="row d-flex justify-content-between align-items-start">
+
+                    {/* Logo and Description */}
+                    <div className="col-lg-4 col-md-12 text-start mb-4 mb-lg-0">
+                        <Link href="/" legacyBehavior>
+                            <Image 
+                                src="/assets/imgs/ccdr-logo.png" 
+                                alt="CCDR Logo" 
+                                width={200}
+                                height={60} 
+                                priority
+                            />
+                        </Link>
+                        <p className="text-muted mt-3" style={{ lineHeight: "1.6" }}>
+                            The Centre for Community Development and Research (CCDR) at Delhi Technological University (DTU) is dedicated to fostering sustainable community development through impactful research, capacity-building initiatives, and collaborative partnerships.
+                        </p>
                     </div>
 
-                    <div className="footer-col-6 col-md-3 col-sm-12" style={{marginLeft:"500px"}}>
-                        <h6 className="mb-20">Download App</h6>
-                        <p className="color-text-paragraph-2 font-xs">Download our Apps and get extra 15% Discount on your first Order…!</p>
-                        <div className="mt-15">
-                            <a className="mr-5" href="#"><img src="assets/imgs/template/icons/app-store.png" alt="joxBox" /></a>
-                            <a href="#"><img src="assets/imgs/template/icons/android.png" alt="joxBox" /></a></div>
+                    {/* Resources Section */}
+                    <div className="col-lg-4 col-md-12 text-center mb-4 mb-lg-0" style={{width:"500px"}}>
+                        <h5 className="fw-bold mb-3">Resources</h5>
+                        <div 
+                            className="d-flex justify-content-center gap-4"
+                            style={{ flexWrap: "wrap" }}
+                        >
+                            <Link href="/index-3" className="text-decoration-none text-dark" style={{ whiteSpace: "nowrap" }}>
+                                About Us
+                            </Link>
+                            <Link href="/index-3" className="text-decoration-none text-dark" style={{ whiteSpace: "nowrap" }}>
+                                Core Members
+                            </Link>
+                            <Link href="/index-3" className="text-decoration-none text-dark" style={{ whiteSpace: "nowrap" }}>
+                                Strategic Goals
+                            </Link>
+                            <Link href="/index-3" className="text-decoration-none text-dark" style={{ whiteSpace: "nowrap" }}>
+                                Contact
+                            </Link>
+                        </div>
+                    </div>
+
+                    {/* Download App Section */}
+                    <div className="col-lg-4 col-md-12 text-end"  style={{width:"200px"}}>
+                        <h5 className="fw-bold mb-3">Download App</h5>
+                        <p className="text-muted">Get our mobile app:</p>
+                        <Link href="https://play.google.com/store" target="_blank" rel="noopener noreferrer">
+                            <Image 
+                                src="/assets/imgs/template/icons/android.png" 
+                                alt="Download App" 
+                                width={150} 
+                                height={50}
+                            />
+                        </Link>
                     </div>
                 </div>
-                <div className="footer-bottom mt-50">
-                    <div className="row">
-                        {/* <div className="col-md-6"><span className="font-xs color-text-paragraph">Copyright © 2022. JobBox all right reserved</span></div> */}
-                        {/* <div className="col-md-6 text-md-end text-start">
-                            <div className="footer-social">
-                                <a className="font-xs color-text-paragraph" href="#">Privacy Policy</a>
-                                <a className="font-xs color-text-paragraph mr-30 ml-30" href="#">Terms &amp; Conditions</a>
-                                <a className="font-xs color-text-paragraph" href="#">Security</a></div>
-                        </div> */}
-                    </div>
+
+                {/* Footer Bottom */}
+                <div className="text-center border-top pt-3 mt-4">
+                    <small className="text-muted">
+                        &copy; {new Date().getFullYear()} CCDR, DTU. All Rights Reserved.
+                    </small>
                 </div>
             </div>
         </footer>
-
     );
 };
 
